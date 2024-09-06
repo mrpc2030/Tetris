94545085844a3d9065a2a03a5101fac78e94f63c
pc1024 <aida_pc@qq.com>
2024-09-07 02:15:31 +0800

    first commit

---
+++ src/components/guide/index.js
@@ -0,0 +1,20 @@
+import { i18n, lan } from '../../unit/const'
+import { isMobile } from '../../unit'
+export default {
+  name: 'Guide',
+  data() {
+    return {
+      isMobile: isMobile()
+    }
+  },
+  computed: {
+  },
+  mounted() {
+    window.addEventListener('resize', this.resize.bind(this), true)
+  },
+  methods: {
+    resize() {
+      this.isMobile = isMobile()
+    }
+  }
+}
